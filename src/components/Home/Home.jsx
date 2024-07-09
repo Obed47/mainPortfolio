ba8f937a0529c792abc5dd8404bbe7d231c77309
obed47 <obedwirngo47@gmail.com>
2024-07-09 04:36:18 +0100

    first push here

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className='Home'>
+            <div className="head">
+                <div className="titleName">
+                    {/* this is the left part of the head side of the website */}
+                    <h1>Hi, I'm Obed</h1>
+
+
+                    <ul>
+                        <li>FrontEnd Developer</li>
+                        <li>Product Designer</li>
+                        <li>Graphic Designer</li>
+                    </ul>
+                
+                    <ul>
+                        <li>ReactJs</li>
+                        <li>HTML & CSS</li>
+                        <li>Figma & Lunacy</li>
+                        <li>Adobe Photoshop & Canva</li>
+                    </ul>
+
+                </div>
+                <div className="imageSpace">
+
+                </div>
+        </div>
+    </div>
+  )
+}
